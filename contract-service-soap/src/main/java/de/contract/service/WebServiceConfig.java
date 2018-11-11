c3c2dfdba6e6d381aead28a592281900c5baea48
javaansehz <javaansehz@gmail.com>
2018-11-11 12:04:45 +0100

    fix wsdl

--- contract-service-soap/src/main/java/de/contract/service/WebServiceConfig.java
+++ contract-service-soap/src/main/java/de/contract/service/WebServiceConfig.java
@@ -4,16 +4,24 @@ import de.contracts.contracts.ContractsPortService;
 import org.apache.cxf.Bus;
 import org.apache.cxf.bus.spring.SpringBus;
 import org.apache.cxf.jaxws.EndpointImpl;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 import javax.xml.ws.Endpoint;
+import java.io.IOException;
 
 
 @Configuration
 public class WebServiceConfig {
 
     private static final String SERVICE_NAME_URL_PATH = "/contracts";
+
+    @Value("classpath:wsdl/contract-service-soap.wsdl")
+    private Resource wsdlResource;
+
 
     @Bean(name=Bus.DEFAULT_BUS_ID)
     public SpringBus springBus() {
@@ -22,15 +30,15 @@ public class WebServiceConfig {
 
 
     @Bean
-    public ContractsPortService contractsPortService() {
-        return new ContractsPortService();
+    public ContractsPortService contractsPortService() throws IOException {
+        return new ContractsPortService(wsdlResource.getURL());
     }
 
     @Bean
-    public Endpoint endpoint(ContractsService contractsService) {
+    public Endpoint endpoint(ContractsService contractsService) throws IOException {
         EndpointImpl endpoint = new EndpointImpl(springBus(), contractsService);
         endpoint.setServiceName(contractsPortService().getServiceName());
-        endpoint.setWsdlLocation(contractsPortService().getWSDLDocumentLocation().toString());
+        endpoint.setWsdlLocation(wsdlResource.getURL().toString());
         endpoint.publish(SERVICE_NAME_URL_PATH);
         return endpoint;
     }

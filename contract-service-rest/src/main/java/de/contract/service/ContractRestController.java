0e7f008391cbc97d2966794a7d330ef00b23f55a
javaansehz <javaansehz@gmail.com>
2018-11-11 11:16:00 +0100

    rest controller

---
+++ contract-service-rest/src/main/java/de/contract/service/ContractRestController.java
@@ -0,0 +1,27 @@
+package de.contract.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.contract.service.api.ContractApi;
+import de.contract.service.model.Contract;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+public class ContractRestController implements ContractApi {
+
+    @RequestMapping(
+            value = {"/contract"},
+            produces = {"application/json"},
+            consumes = {"application/json"},
+            method = {RequestMethod.POST}
+    )
+    public ResponseEntity<Contract> calulateContract(@Valid @RequestBody Contract body) {
+        return new ResponseEntity(body, HttpStatus.OK);
+    }
+}

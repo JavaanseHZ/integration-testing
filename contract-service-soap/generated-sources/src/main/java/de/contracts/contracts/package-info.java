f7cab6217469df2e294c8e21b982de2c2709f5c7
javaansehz <javaansehz@gmail.com>
2018-11-11 05:46:38 +0100

    generate code form wsdl

--- contract-service-soap/generated-sources/src/main/java/de/contracts/contracts/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://contracts.de/contracts", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package de.contracts.contracts;

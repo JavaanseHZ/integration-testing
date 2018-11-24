dc344e1fd83ed5572659e4e16dd4675130931c32
javaansehz <javaansehz@gmail.com>
2018-11-24 08:25:05 +0100

    over -> during

--- karate/src/gatling/simulations/contract/ContractKarateGatling.scala
+++ karate/src/gatling/simulations/contract/ContractKarateGatling.scala
@@ -4,6 +4,7 @@ import com.intuit.karate.gatling.KarateProtocol
 import com.intuit.karate.gatling.PreDef._
 import io.gatling.core.Predef._
 import io.gatling.core.structure.ScenarioBuilder
+import scala.concurrent.duration._
 
 class ContractKarateGatling extends Simulation {
 
@@ -15,8 +16,11 @@ class ContractKarateGatling extends Simulation {
     .exec(karateFeature("classpath:contract/contract-risk-rest.feature"))
     .pause(5)
 
-  setUp(
-    riskMedium.inject(atOnceUsers(1)).protocols(protocol)
-  )
+  setUp(riskMedium.inject(rampUsers(5) during(5 seconds))
+  ).protocols(protocol).maxDuration(1 minutes).assertions(global.responseTime.mean.lt(1000))
+
+  after {
+    println("Simulation is finished!")
+  }
 
 }
